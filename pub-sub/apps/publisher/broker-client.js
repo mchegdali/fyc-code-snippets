80239dce5cea772bce19993a61cb899efff259e1
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-12-03 23:28:26 +0100

    refactor branches

--- pub-sub/apps/publisher/broker-client.js
+++
@@ -1,7 +0,0 @@
-import PubSubBrokerClient from "../../common/PubSubBrokerClient.js";
-
-const brokerClient = new PubSubBrokerClient({
-  subscriberUrl: "http://localhost:3000",
-});
-
-export default brokerClient;
