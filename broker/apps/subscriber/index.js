79c0785996ed99c46f5569dbf89a0538281b351c
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-12-03 22:40:23 +0100

    initial commit

--- broker/apps/subscriber/index.js
+++ broker/apps/subscriber/index.js
@@ -5,22 +5,50 @@ import brokerClient from "../publisher/broker-client.js";
 const app = express();
 app.use(express.json());
 
+const KNOWN_CHANNELS = ["logs", "notifications"];
+
+const LOG_LEVELS = ["info", "warn", "error"];
+
 app.get("/subscribe", async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const channel = url.searchParams.get("channel");
 
   if (!channel) {
-    return res.status(400).json({ error: "Canal non spécifié" });
+    res.status(400).json({ error: "Canal non spécifié" });
+    return;
   }
 
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
 
-  res.write(`Vous êtes abonné au canal : ${channel}\n\n`);
+  if (!KNOWN_CHANNELS.includes(channel)) {
+    res.write(`Vous êtes abonné au canal : ${channel}\n\n`);
+  }
 
   const callback = (message) => {
-    res.write(`data: ${JSON.stringify({ channel, message })}\n\n`);
+    let body = "";
+
+    if (KNOWN_CHANNELS.includes(channel)) {
+      switch (channel) {
+        case "logs":
+          const randomLogLevel =
+            LOG_LEVELS[Math.floor(Math.random() * LOG_LEVELS.length)];
+          body = JSON.stringify({
+            level: randomLogLevel,
+            ts: Date.now(),
+            message,
+            channel,
+          });
+          break;
+        default:
+          body = JSON.stringify({ channel, message });
+      }
+    } else {
+      body = JSON.stringify({ channel, message });
+    }
+
+    res.write(`data: ${body}\n\n`);
   };
 
   await brokerClient.subscribe(channel, callback);
@@ -33,17 +61,17 @@ app.get("/subscribe", async (req, res) => {
 });
 
 app.post("/notify", (req, res) => {
-  console.log("subscriber/notify/body", req.body);
-
   const { channel, message } = req.body;
 
   if (!channel || !message) {
-    return res.status(400).json({ error: "Channel and message are required" });
+    res.status(422).json({ error: "Canal ou message manquant" });
+    return;
   }
+
+  brokerClient.emit(channel, message);
 
   console.log(`Message reçu sur le canal ${channel}:`, message);
 
-  // sendToChannel(channel, message);
   res.status(200).end();
 });
 
