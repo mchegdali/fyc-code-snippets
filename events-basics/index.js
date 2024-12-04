7f8b198c1968d1102eb080e8788ddeb872ae39d3
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-12-04 02:27:26 +0100

    sync-async ok

--- events-basics/index.js
+++
@@ -1,8 +0,0 @@
-import { EventEmitter } from "node:events";
-const emitter = new EventEmitter();
-
-emitter.on("connect", () => {
-  console.log("Connexion établie !");
-});
-
-emitter.emit("connect");
