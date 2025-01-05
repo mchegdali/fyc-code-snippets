4f7b9e7b847f02599860e2871a5181aa28de02b6
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2025-01-05 15:40:50 +0100

    refactor(pub-sub): simplify greatly the exercice

--- pub-sub/apps/broker/index.js
+++
@@ -1,6 +0,0 @@
-import PubSubBrokerServer from "../../common/PubSubBrokerServer.js";
-
-const PORT = 3002;
-const server = new PubSubBrokerServer();
-
-server.listen(PORT);
--- pub-sub/apps/publisher/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  PORT: 3001,
-  brokerUrl: "http://localhost:3002",
-  get subscriberUrl() {
-    return `http://localhost:${this.PORT}`;
-  },
-};
-
-export default config;
