9cc764bf0d4127fb4f82ae327e5b284402719dc8
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2025-01-07 19:30:18 +0100

    fix(events-basics): remove useless file

--- apps/events-basics/test.js
+++
@@ -1,9 +0,0 @@
-import EventEmitter from "node:events";
-import { createServer } from "node:http";
-
-const server = createServer((req, res) => {
-  res.writeHead(200);
-  res.end("Hello World\n");
-});
-
-console.log(server instanceof EventEmitter);
